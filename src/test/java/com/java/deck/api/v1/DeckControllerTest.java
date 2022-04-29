f4d5f7355d548ff91e8d21e3d6f8734d54d2ac22
Carlos Matus <c-carlos.matus@charter.com>
2022-04-29 16:06:41 -0500

    adding docker image

--- src/test/java/com/java/deck/api/v1/DeckControllerTest.java
+++ src/test/java/com/java/deck/api/v1/DeckControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.http.ResponseEntity;
 
 import static org.mockito.Mockito.verify;
 
--- src/test/java/com/java/deck/service/DeckServiceTest.java
+++ src/test/java/com/java/deck/service/DeckServiceTest.java
@@ -1,25 +1,18 @@
 package com.java.deck.service;
 
 import com.java.deck.model.Card;
-import com.java.deck.model.Deck;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.AbstractMap;
 import java.util.Map.Entry;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DeckServiceTest {
 
-    @Mock
-    private Deck deck;
     @InjectMocks DeckService deckService;
 
     @Test
@@ -28,10 +21,6 @@ public class DeckServiceTest {
         Card card = new Card("2", "diamonds");
         card.setFaceName(faceName);
 
-        Entry<String, Card> entry = new AbstractMap.SimpleImmutableEntry<>("123", card);
-
-
-        when(deck.getNextCard(anyString())).thenReturn(entry);
         String deckId = deckService.generateNewDeck();
         final Entry<String, Card> result = deckService.getNextCard(deckId, null);
 
@@ -41,8 +30,6 @@ public class DeckServiceTest {
     @Test
     public void skipNextCard() {
 
-        String newCardIndex = "12345";
-        when(deck.spikNextCard(anyString())).thenReturn(newCardIndex);
         String deckId = deckService.generateNewDeck();
         String cardIndex  = deckService.skipNextCard(deckId, null);
 
